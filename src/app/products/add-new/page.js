8a1ee9298745cb54432bf8658eb1fe71a8dbcbcd
Aleksandar Vasilev <alexander.vasilev@protonmail.com>
2023-03-20 19:45:56 +0200

    adding few comments

--- src/app/products/add-new/page.js
+++ src/app/products/add-new/page.js
@@ -1,29 +1,71 @@
 "use client"
-
 import axios from "axios";
+import {useState} from "react";
 
-async function addNewProduct() {
-    axios.post('http://localhost:3000/api/product', 'data')
-        .then(a => {
-            console.log(a)
-        })
-        .catch(e => {
-            console.log(e)
-        });
-}
-
-const productData = {
-    name: '',
-    productPrice: 0,
-    tax: 0,
-    finalPrice: 0,
-    cost: 0,
-    preparationTime: 0,
-    shelfLife: 0,
-    isResell: false,
-    notes: '',  
-}
 export default function AddNew() {
+
+    const [productData, setProductData] = useState({
+        name: '',
+        price: 0,
+        tax: 0,
+        finalPrice: 0,
+        cost: 0,
+        preparationTime: 0,
+        shelfLife: 0,
+        isResell: false,
+        notes: '',
+    });
+
+    const [isLoading, setIsLoading] = useState(false);
+
+    // Handles all the changes for the input fields and updates the productData
+    function handleChange(e) {
+        let finalPrice = productData.finalPrice;
+        if (e.target.name === 'price' || e.target.name === 'tax') {
+            if (e.target.name === 'price') {
+                finalPrice = ((1 + productData.tax) * e.target.value).toFixed(2);
+            } else {
+                finalPrice = ((1 + (e.target.value / 100)) * productData.price).toFixed(2);
+            }
+        }
+        setProductData({
+            ...productData,
+            [e.target.name]: e.target.value,
+            finalPrice: finalPrice
+        });
+    }
+
+    // Tracks the changes of the checkbox
+    function handleCheckboxChange(e) {
+        setProductData({
+            ...productData,
+            isResell: e.target.checked,
+        });
+    }
+    // Monitors when the data is being sent to the database
+    function handleIsLoading(status) {
+        setIsLoading({
+            isLoading: status
+        });
+    }
+
+    function addNewProduct(productData) {
+        handleIsLoading(true);
+        const dataCopy = {...productData};
+        console.log(dataCopy)
+        const data = JSON.stringify(...productData)
+        console.log(data);
+        axios.post('http://localhost:3000/api/product', data)
+            .then(a => {
+                handleIsLoading(false);
+                console.log(a)
+            })
+            .catch(e => {
+                handleIsLoading(false);
+                console.log(e)
+            });
+    }
+
     return (
         <main>
             <div className="w-1/3 mx-auto max-w-screen-sm">
@@ -33,6 +75,9 @@ export default function AddNew() {
                     <label className="block">
                         Product name
                         <input type="text"
+                               name="name"
+                               value={productData.name}
+                               onChange={handleChange}
                                className="block w-96 border-1 rounded border-gray-500 ring-offset-2 ring-2 shadow-sm
                                focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
                                placeholder=""/>
@@ -41,6 +86,9 @@ export default function AddNew() {
                     <label className="block">
                         Product price (without tax)
                         <input type="number"
+                               name="price"
+                               value={productData.price}
+                               onChange={handleChange}
                                className="block w-24 border-1 rounded border-gray-500 ring-offset-2 ring-2 shadow-sm
                                focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
                                placeholder="0"/>
@@ -49,6 +97,9 @@ export default function AddNew() {
                     <label className="block">
                         Tax/VAT (%)
                         <input type="number"
+                               name="tax"
+                               value={productData.tax}
+                               onChange={handleChange}
                                className="block w-24 border-1 rounded border-gray-500 ring-offset-2 ring-2 shadow-sm
                                focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
                                placeholder="0.0"/>
@@ -56,15 +107,21 @@ export default function AddNew() {
 
                     <label className="block">
                         Final price
-                        <input type="number"
-                               className="block w-24 border-1 rounded border-gray-500 ring-offset-2 ring-2 shadow-sm
-                               focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                        <input type="text"
+                               name="finalPrice"
+                               value={productData.finalPrice}
+                               readOnly
+                               disabled
+                               className="block w-24 border-1 rounded border-gray-500 ring-offset-2 ring-2 shadow-sm disabled:opacity-75 read-only:bg-gray-100"
                                placeholder="0"/>
                     </label>
 
                     <label className="block">
                         Preparation time (hours)
                         <input type="number"
+                               name="preparationTime"
+                               value={productData.preparationTime}
+                               onChange={handleChange}
                                className="block w-24 border-1 rounded border-gray-500 ring-offset-2 ring-2 shadow-sm
                                focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
                                placeholder="1"/>
@@ -73,6 +130,9 @@ export default function AddNew() {
                     <label className="block">
                         Shelf life (hours)
                         <input type="number"
+                               name="shelfLife"
+                               value={productData.shelfLife}
+                               onChange={handleChange}
                                className="block w-24 border-1 rounded border-gray-500 ring-offset-2 ring-2 shadow-sm
                                focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
                                placeholder="42"/>
@@ -80,6 +140,9 @@ export default function AddNew() {
 
                     <label className="block">
                         <input type="checkbox"
+                               name="isResell"
+                               checked={productData.isResell}
+                               onChange={handleCheckboxChange}
                                className="accent-pink-300 focus:accent-pink-500 mr-2"/>
                         This a reselling product
                     </label>
@@ -88,6 +151,9 @@ export default function AddNew() {
                     <label className="block">
                         Additional notes
                         <textarea
+                            name="notes"
+                            value={productData.notes}
+                            onChange={handleChange}
                             className="block mt-1 w-full
                                 border-1 border-gray-300 border-solid rounded shadow-md
                                 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
@@ -99,7 +165,9 @@ export default function AddNew() {
                 </div>
 
                 <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                        onClick={addNewProduct}>
+                        onClick={addNewProduct}
+                        disabled={isLoading}
+                >
                     Add new product
                 </button>
             </div>
