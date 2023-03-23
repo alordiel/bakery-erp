b876427e22e3c5e3ee2c1b3da7c7fb53e8303d6a
alordiel <alexander.vasilev@protonmail.com>
2023-03-23 14:37:54 +0200

    small stylings on the products page

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -11,36 +11,41 @@ async function getUserData() {
         });
 }
 
-async function deleteProduct(){
+async function deleteProduct() {
 
 }
 
 function ProductsList({allProducts}) {
-     if (allProducts.products !== undefined && allProducts.products.length > 0) {
-         const productRows = allProducts.products.map( product => {
-             return <tr key={product.id}>
-                 <td className="border border-slate-300 dark:border-slate-700 p-4 text-slate-500 dark:text-slate-400">{product.name }</td>
-                 <td className="border border-slate-300 dark:border-slate-700 p-4 text-slate-500 dark:text-slate-400">{product.cost}</td>
-                 <td className="border border-slate-300 dark:border-slate-700 p-4 text-slate-500 dark:text-slate-400">{product.finalPrice}</td>
-                 <td className="border border-slate-300 dark:border-slate-700 p-4 text-slate-500 dark:text-slate-400">
-                     <Link href={"/product/" + product.id}>edit</Link> &nbsp;
-                     <a href="#">delete</a>
-                 </td>
-             </tr>
-         })
-               return <table className="border-collapse w-full border border-slate-400 dark:border-slate-500 bg-white dark:bg-slate-800 text-sm shadow-sm">
-                    <thead className="bg-slate-50 dark:bg-slate-700">
-                    <tr>
-                        <th className="border border-slate-300 dark:border-slate-600 font-semibold p-4 text-slate-900 dark:text-slate-200 text-left">Product name</th>
-                        <th className="border border-slate-300 dark:border-slate-600 font-semibold p-4 text-slate-900 dark:text-slate-200 text-left">Cost</th>
-                        <th className="border border-slate-300 dark:border-slate-600 font-semibold p-4 text-slate-900 dark:text-slate-200 text-left">Final price</th>
-                        <th className="border border-slate-300 dark:border-slate-600 font-semibold p-4 text-slate-900 dark:text-slate-200 text-left">Actions</th>
-                    </tr>
-                    </thead>
-                    <tbody>{productRows}</tbody>
-                </table>
-            }
-               return <p>No Products found.</p>
+    if (allProducts.products !== undefined && allProducts.products.length > 0) {
+        const productRows = allProducts.products.map(product => {
+            return <tr key={product.id}>
+                <td className="border border-slate-300 dark:border-slate-700 p-4 text-slate-500 dark:text-slate-400">{product.name}</td>
+                <td className="border border-slate-300 dark:border-slate-700 p-4 text-slate-500 dark:text-slate-400">{product.cost}</td>
+                <td className="border border-slate-300 dark:border-slate-700 p-4 text-slate-500 dark:text-slate-400">{product.finalPrice}</td>
+                <td className="border border-slate-300 dark:border-slate-700 p-4 text-slate-500 dark:text-slate-400">
+                    <Link href={"/product/" + product.id}>edit</Link> &nbsp;
+                    <a href="#">delete</a>
+                </td>
+            </tr>
+        })
+        return <table
+            className="border-collapse w-full border border-slate-400 dark:border-slate-500 bg-white dark:bg-slate-800 text-sm shadow-sm">
+            <thead className="bg-slate-50 dark:bg-slate-700">
+            <tr>
+                <th className="border border-slate-300 dark:border-slate-600 font-semibold p-4 text-slate-900 dark:text-slate-200 text-left">Product
+                    name
+                </th>
+                <th className="border border-slate-300 dark:border-slate-600 font-semibold p-4 text-slate-900 dark:text-slate-200 text-left">Cost</th>
+                <th className="border border-slate-300 dark:border-slate-600 font-semibold p-4 text-slate-900 dark:text-slate-200 text-left">Final
+                    price
+                </th>
+                <th className="border border-slate-300 dark:border-slate-600 font-semibold p-4 text-slate-900 dark:text-slate-200 text-left">Actions</th>
+            </tr>
+            </thead>
+            <tbody>{productRows}</tbody>
+        </table>
+    }
+    return <p>No Products found.</p>
 
 }
 
@@ -55,11 +60,13 @@ export default async function Production() {
 
     return (
         <main>
-            <h1>Продукти</h1>
+            <h1 className="text-4xl">Продукти</h1>
             <br/>
-            <ProductsList
-               allProducts={allProducts}
-            ></ProductsList>
+            <div className="mb-5">
+                <ProductsList
+                    allProducts={allProducts}
+                ></ProductsList>
+            </div>
             <Link href={"/products/add-new"}
                   className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                 Add new product
